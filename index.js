11f22c5605bd161c3fe133948fff5a4091ef375b
Christof Röhl <userpattern@gmail.com>
2024-01-26 11:44:38 +0100

    solved

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const company = {
   location: "Hamburg",
 };
 
+export const { course } = company;
 // example: export const {value1} = myObject;
 
 // EXERCISE 2
@@ -20,6 +21,9 @@ const company = {
 //  2.3: 'isAdmin' property into the variable 'isAdmin' (false, if no such property exists)
 
 const user = { name: "John", years: 30 };
+export const { name, years: age, isAdmin = false } = user;
+console.log(name);
+console.log(age);
 
 // EXERCISE 3
 // Extract the variables from the dog object and
@@ -30,7 +34,7 @@ const dog = {
   breed: "husky",
   age: 5,
 };
-
+export const { name: dogName, breed: dogBreed, age: dogAge } = dog;
 // EXERCISE 4
 // Extract the 'lastName' property from the person object as "personLastName".
 // Store all other properties in an object called "moreInformation".
@@ -40,3 +44,6 @@ const person = {
   age: 24,
   firstName: "Alex",
 };
+export const { lastName: personLastName } = person;
+export const { lastName, ...moreInformation } = person;
+console.log(moreInformation);
